0ff8bc573c98281a0e7c9f14f5170ae3296cbab2
Yeung Pui Yu <yeungpuiyu@yeungs-air.lan>
2018-04-22 07:48:03 +0800

    Initial Commit

---
+++ HeaX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeaX
+//
+//  Created by Yeung Pui Yu on 12/4/2018.
+//  Copyright © 2018 Yeung Pui Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
